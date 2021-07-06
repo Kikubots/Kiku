cad31255b3b08fbbb133fe8a0d2a937006d2cbb6
Kikubots <86219720+Kikubots@users.noreply.github.com>
2021-07-06 22:25:36 +0530

    Update yourstepsis.py

--- yourstepsis.py
+++ yourstepsis.py
@@ -1,6 +1,6 @@
 from os import system
-system("git clone https://gitHub.com/mdnoor786/Lion-X && cd Lion-X && python3 -m Lion")
+system("git clone https://gitHub.com/kikubots/kiku && cd kiku && python3 -m kiku")
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @teamkiku")
 # Script written for stop heroku suspension
 # use with credit else gay
