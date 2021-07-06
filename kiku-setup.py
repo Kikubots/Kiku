33b1b569672849cdb111b1f3c717c8b6ddd719ce
Kikubots <86219720+Kikubots@users.noreply.github.com>
2021-07-06 22:30:03 +0530

    Update and rename Lion-setup.py to kiku-setup.py

--- kiku-setup.py
+++ kiku-setup.py
@@ -11,16 +11,17 @@ print("""𝙿𝙻𝙴𝙰𝚂𝙴 𝙶𝙾 𝚃𝙾 my.telegram.org
 𝙻𝙾𝙶𝙸𝙽 𝚄𝚂𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 𝙰𝙲𝙲𝙾𝚄𝙽𝚃
 𝙲𝙻𝙸𝙲𝙺 𝙾𝙽 𝙰𝙿𝙸 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙼𝙴𝙽𝚃 𝚃𝙾𝙾𝙻𝚂
 𝙲𝚁𝙴𝙰𝚃𝙴 𝙰 𝙽𝙴𝚆 𝙰𝙿𝙿𝙻𝙸𝙲𝙰𝚃𝙸𝙾𝙽, 𝙱𝚈 𝙴𝙽𝚃𝙴𝚁𝙸𝙽𝙶 𝚁𝙴𝚀𝚄𝙸𝚁𝙴𝙳 𝙳𝙴𝚃𝙰𝙸𝙻𝚂
-𝚃𝙴𝙰𝙼 𝙻𝙸𝙾𝙽
- _       _____   ____   _   _
-| |     |_   _| / __ \\ | \\ | |
-| |       | |  | |  | ||  \\| |
-| |       | |  | |  | || . ` |
-| |____  _| |_ | |__| || |\\  |
-|______||_____| \\____/ |_| \\_|
+𝙺𝙸𝙺𝚄 𝙻𝙸𝙾𝙽
+
+┏┓┏━┳━━┳┓┏━┳┓╋┏┓
+┃┃┃┏┻┫┣┫┃┃┏┫┃╋┃┃
+┃┗┛┛╋┃┃┃┗┛┛┃┃╋┃┃
+┃┏┓┃╋┃┃┃┏┓┃┃┃╋┃┃
+┃┃┃┗┳┫┣┫┃┃┗┫┗━┛┃
+┗┛┗━┻━━┻┛┗━┻━━━┛
 
 
-Running Lion Fire on Termux 🔥🔥🔥🔥....
+Running kiku Fire on Termux 🔥🔥🔥🔥....
 """)
 print("")
 
@@ -30,9 +31,9 @@ API_HASH = input("𝙴𝙽𝚃𝙴𝚁 𝚈𝙾𝚄𝚁 𝙰𝙿𝙸 𝙷𝙰�
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     tele = client.send_message("me", client.session.save())
     tele.reply(
-        "✘ Hᴇʀᴇ ɪs ʏᴏᴜʀ `LION_SESSION` Oғ ʟɪᴏɴ ᴜsᴇʀʙᴏᴛ ✘.\n@LionXSupport")
+        "✘ Hᴇʀᴇ ɪs ʏᴏᴜʀ `KIKU_SESSION` Oғ ʟɪᴏɴ ᴜsᴇʀʙᴏᴛ ✘.\n@kikusupport")
     print("")
-    print("Bᴇʟᴏᴡ ɪs ʏᴏᴜʀ LION_SESSION. Wᴇ ʜᴀᴠᴇ ᴀʟsᴏ sᴛᴏʀᴇᴅ ɪɴ ᴛᴇʟᴇɢʀᴀᴍ sᴀᴠᴇ ᴍᴇssᴀɢᴇs")
+    print("Bᴇʟᴏᴡ ɪs ʏᴏᴜʀ KIKU_SESSION. Wᴇ ʜᴀᴠᴇ ᴀʟsᴏ sᴛᴏʀᴇᴅ ɪɴ ᴛᴇʟᴇɢʀᴀᴍ sᴀᴠᴇ ᴍᴇssᴀɢᴇs")
     print("")
     print("")
     print(client.session.save())
