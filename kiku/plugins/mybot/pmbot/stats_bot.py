15a0e75d77516fb1b619a9a07e582286072de151
Kikubots <86219720+Kikubots@users.noreply.github.com>
2021-07-06 23:07:02 +0530

    Update stats_bot.py

--- kiku/plugins/mybot/pmbot/stats_bot.py
+++ kiku/plugins/mybot/pmbot/stats_bot.py
@@ -1,5 +1,5 @@
-#    Lion - UserBot
-#    Copyright (C) 2020 Lion
+#    kiku - UserBot
+#    Copyright (C) 2020 kiku
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
