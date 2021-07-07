771c7a5931aa1d080f0ca23b6b42f9ceecd8bebc
Kikubots <86219720+Kikubots@users.noreply.github.com>
2021-07-07 10:04:19 +0530

    Update json.py

--- kiku/plugins/json.py
+++ kiku/plugins/json.py
@@ -2,12 +2,12 @@
 Syntax: .json"""
 import io
 
-from Lion import CMD_HELP
-from Lion.utils import admin_cmd
+from kiku import CMD_HELP
+from kiku.utils import admin_cmd
 
 
-@Lion.on(admin_cmd(pattern="json"))
-@Lion.on(sudo_cmd(pattern="json", allow_sudo=True))
+@kiku.on(admin_cmd(pattern="json"))
+@kiku.on(sudo_cmd(pattern="json", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
