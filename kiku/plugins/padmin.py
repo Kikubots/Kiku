4c4bcbe1d97b686ff99ef6e8a75639613b59015c
Kikubots <86219720+Kikubots@users.noreply.github.com>
2021-07-07 09:12:19 +0530

    Update padmin.py

--- kiku/plugins/padmin.py
+++ kiku/plugins/padmin.py
@@ -7,10 +7,10 @@ Available Commands:
 
 import asyncio
 
-from Lion.utils import admin_cmd
+from kiku.utils import admin_cmd
 
 
-@Lion.on(admin_cmd(pattern=r"(.*)", outgoing=True))
+@kiku.on(admin_cmd(pattern=r"(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
