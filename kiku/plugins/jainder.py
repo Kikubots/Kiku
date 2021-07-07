2965cdc9f4cb47a639b1e87ab42c7ddc3faf4d94
Kikubots <86219720+Kikubots@users.noreply.github.com>
2021-07-07 10:05:51 +0530

    Update jainder.py

--- kiku/plugins/jainder.py
+++ kiku/plugins/jainder.py
@@ -3,11 +3,11 @@
 
 import random
 
-from Lion import CMD_HELP
-from Lion.utils import admin_cmd
+from kiku import CMD_HELP
+from kiku.utils import admin_cmd
 
 
-@Lion.on(admin_cmd(pattern=r"jainder(.*)", outgoing=True))
+@kiku.on(admin_cmd(pattern=r"jainder(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
