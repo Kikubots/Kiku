a993b1d75b196d80940077b4e16bd30061cb3228
Kikubots <86219720+Kikubots@users.noreply.github.com>
2021-07-07 09:46:12 +0530

    Update marveldp.py

--- kiku/plugins/marveldp.py
+++ kiku/plugins/marveldp.py
@@ -16,8 +16,8 @@ import urllib
 import requests
 from telethon.tl import functions
 
-from Lion import CMD_HELP
-from Lion.utils import admin_cmd
+from kiku import CMD_HELP
+from kiku.utils import admin_cmd
 
 COLLECTION_STRING = [
     "avengers-logo-wallpaper",
@@ -76,7 +76,7 @@ async def animepp():
     urllib.request.urlretrieve(fy, "donottouch.jpg")
 
 
-@Lion.on(admin_cmd(pattern="marveldp ?(.*)"))
+@kiku.on(admin_cmd(pattern="marveldp ?(.*)"))
 async def main(event):
 
     await event.edit(
