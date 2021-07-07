f39924e513cd8e96d8caa962b0d7126ec0bd1951
Kikubots <86219720+Kikubots@users.noreply.github.com>
2021-07-07 08:03:23 +0530

    Update tts.py

--- kiku/plugins/tts.py
+++ kiku/plugins/tts.py
@@ -11,11 +11,11 @@ from datetime import datetime
 
 from gtts import gTTS
 
-from Lion.utils import admin_cmd
+from kiku.utils import admin_cmd
 
 
-@Lion.on(admin_cmd(pattern="tts (.*)"))
-@Lion.on(sudo_cmd(pattern="tts (.*)", allow_sudo=True))
+@kiku.on(admin_cmd(pattern="tts (.*)"))
+@kiku.on(sudo_cmd(pattern="tts (.*)", allow_sudo=True))
 async def _(event):
 
     if event.fwd_from:
