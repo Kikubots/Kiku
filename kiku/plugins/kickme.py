b93629ea2b4dac463a09b6f7e3ab4250a84e97e1
Kikubots <86219720+Kikubots@users.noreply.github.com>
2021-07-07 10:03:41 +0530

    Update kickme.py

--- kiku/plugins/kickme.py
+++ kiku/plugins/kickme.py
@@ -13,11 +13,11 @@ import time
 
 from telethon.tl.functions.channels import LeaveChannelRequest
 
-from Lion import CMD_HELP
-from Lion.utils import admin_cmd
+from kiku import CMD_HELP
+from kiku.utils import admin_cmd
 
 
-@Lion.on(admin_cmd(pattern="kickme", outgoing=True))
+@kiku.on(admin_cmd(pattern="kickme", outgoing=True))
 async def leave(e):
     x = bot.me
     name = x.first_name
